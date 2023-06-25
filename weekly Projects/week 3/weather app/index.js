e5e7fb06b61ef437dcdb0b928a0fb5164516773a
josiahngugi <mainajosiahdev@gmail.com>
2023-06-25 11:45:44 -0700

    fetch data from APi and render data

---
+++ weekly Projects/week 3/weather app/index.js
@@ -0,0 +1,37 @@
+
+const API_Key ="d9f5399c4ab74bc48a4101954232406"
+
+
+async function  getWeatherData(city){
+    const API =`http://api.weatherapi.com/v1/current.json?key=${API_Key}&q=${city}&aqi=no`
+    try{
+        const res =  await fetch(API)
+        const data = await res.json()
+        return data
+    }catch(err){
+        console.log(err.message)
+        return null
+    }
+
+}
+
+function displayData(data){
+    const div =document.getElementById("display")
+    if(data){
+        const{temp_c, condition } = data.current
+        document.getElementById("display").innerHTML=`<h1>location: ${data.location.name} humidity:${temp_c} cloud: ${condition.text} <img src=${condition.icon} ></h1>`
+    }else{
+        document.getElementById("display").innerHTML="an error has occured"
+    }
+    
+}
+
+function init(){
+    const city =document.getElementById("inputText").value
+    getWeatherData(city)
+    .then((data)=>{displayData(data)})
+    .catch((error)=>{console.log('error', error)})
+}
+
+
+document.getElementById("submit").addEventListener("click", init)
